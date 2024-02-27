0312233326bb54161061434e49342c038edcee87
inayarhmns <inaya.rahmanisa@gmail.com>
2024-02-27 11:03:34 +0700

    feat: profile in my learning page

--- src/sections/about/AboutUs.jsx
+++ src/sections/about/AboutUs.jsx
@@ -5,44 +5,63 @@ const AboutUs = () => {
 
 
   return (
-    <section className="text-primary-green  lg:p-10">
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div className="p-20 font-bold text-5xl">
-            About Us 
+    <section className="text-primary-green p-10 lg:px-48">
+    <div className="grid grid-cols-1  gap-4 px-8 ">
+        <div className="shadow-xl py-20 px-36 font-bold text-5xl h-[250px]flex rounded-sm  text-left flex-col  space-y-4 text-primary-yellow  bg-[url('/bg/about/vision.svg')] bg-cover justify-center bg-no-repeat bg-center">
+            <div className="rounded-md px-10 py-5 w-max bg-primary-green text-white bg-opacity-100">
+                About Us  
+            </div>   
         </div>
-        <div className="pt-5 sm:pr-10 space-y-4">
-            <div className="hover:shadow-xl shadow-md rounded-sm space-y-4 text-white p-5 w-full bg-primary-green">   
-                <p className="text-xl font-bold ">Our Why</p>
-                <p>
+        <div class="px-5 grid grid-cols-3 hover:shadow-3xl shadow-lg rounded-sm">
+            <div class="py-10 pl-10 col-span-1 font-bold text-4xl">
+            Our Why
+            </div>
+            <div class="col-span-2 py-10 pr-10">
                 MOTIVE ingin memberikan solusi kepada para pemuda muslim untuk dapat meraih potensi terbaiknya, terutama dalam ranah pengembangan diri dan ilmu-ilmu keislaman
+            </div>
+        </div>
+        <div class="px-5 grid grid-cols-3 hover:shadow-3xl shadow-lg rounded-sm">
+            <div class=" col-span-2 py-10 pl-10 text-right">
+            MOTIVE akan mewujudkannya dengan menghadirkan pembelajaran yang kompeherensif dan membuka ruang bagi pemuda muslim untuk belajar dan bertumbuh 
+            </div>
+            <div class="text-right col-span-1 py-10 pr-10 font-bold text-4xl">
+            Our How           </div>
+        </div>
+        <div className="grid grid-cols-1 gap-1">
+            <div className="p-10 grid grid-cols-1 text-center hover:shadow-3xl shadow-lg bg-primary-green">
+            <p className="text-3xl font-bold text-primary-yellow">
+                Our Vision
+                </p>
+                <p className="text-white">
+                Menjadi platform edukasi terbesar di Indonesia yang berfokus dalam mempersiapkan generasi pemuda Islam yang berkompeten dan ber-tsaqafah Islamiyah
                 </p>
             </div>
-            <div className="text-right hover:shadow-xl shadow-md rounded-sm space-y-4  p-5 w-full bg-white">   
-            <p className="text-xl font-bold">Our How</p>
-                <p>
-                MOTIVE akan mewujudkannya dengan menghadirkan pembelajaran yang kompeherensif dan membuka ruang bagi pemuda muslim untuk belajar dan bertumbuh                </p>
-            </div>
-        </div> 
-    </div>
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-0 sm:px-10 py-5">
-        <div className="rounded-sm shadow-xl text-center px-5 sm:px-24 flex flex-col  justify-center items-center space-y-4 text-white p-10  bg-[url('/bg/about/vision.svg')] bg-cover bg-center " >
-            <p className="text-3xl font-bold text-primary-yellow">
-            Our Vision
-            </p>
-            <p>
-            Menjadi platform edukasi terbesar di Indonesia yang berfokus dalam mempersiapkan generasi pemuda Islam yang berkompeten dan ber-tsaqafah Islamiyah
-            </p>
-        </div>
-        <div className="rounded-sm shadow-xl px-5 sm:px-20  flex flex-col justify-center items-center space-y-6 text-white p-10 bg-[url('/bg/about/mission.svg')] bg-cover bg-center " >
-            <p className="text-3xl font-bold text-primary-yellow">
+            <div className="p-10 grid grid-cols-1 text-center hover:shadow-3xl shadow-lg  bg-primary-green">
+            <p className="text-3xl font-bold text-primary-yellow pb-8">
             Our Mission
             </p>
-            <ol class="text-center space-y-2 font-reguler text-white">
-                <li className="hover:text-black hover:bg-primary-yellow rounded-md py-2 px-4 bg-primary-green bg-opacity-80" >Menyediakan berbagai skema pembelajaran yang menggabungkan antara keilmuan islam dengan keilmuan praktis</li>
-                <li className="hover:text-black hover:bg-primary-yellow rounded-md py-2 px-4 bg-primary-green bg-opacity-80">Memberikan pelayanan dan pengalaman terbaik pada pemuda Islam dalam menjalani proses pembelajaran dan pengembangan diri</li>
-                <li className="hover:text-black hover:bg-primary-yellow rounded-md py-2 px-4 bg-primary-green bg-opacity-80">Memberikan ruang bagi para pemuda Islam untuk berbagi ilmu, pengalaman, ide, dan gagasan</li>
-                <li className="hover:text-black hover:bg-primary-yellow rounded-md py-2 px-4 bg-primary-green bg-opacity-80">Memperluas penyebaran dampak dengan pemanfaatan media informasi yang kredibel dan informatif</li>
-            </ol>
+            <div class="grid grid-cols-2 gap-0">
+            <div>
+                <ol class="px-10 text-center space-y-2 font-reguler text-white">
+                <li class="flex justify-center items-center text-black hover:bg-primary-yellow rounded-md py-2 px-4 bg-primary-yellow  h-24">
+                    <div>
+                    Menyediakan berbagai skema pembelajaran yang menggabungkan antara keilmuan islam dengan keilmuan praktis
+                    </div>
+                </li>
+                <li class="flex justify-center items-center text-black hover:bg-primary-yellow rounded-md py-2 px-4 bg-primary-yellow  h-24">Memberikan pelayanan dan pengalaman terbaik pada pemuda Islam dalam menjalani proses pembelajaran dan pengembangan diri</li>
+                </ol>
+            </div>
+            <div>
+                <ol class="px-10 text-center space-y-2 font-reguler text-white">
+                <li class="flex justify-center items-center text-black hover:bg-primary-yellow rounded-md py-2 px-4 bg-primary-yellow h-24">Memberikan ruang bagi para pemuda Islam untuk berbagi ilmu, pengalaman, ide, dan gagasan</li>
+                <li class="flex justify-center items-center text-black hover:bg-primary-yellow rounded-md py-2 px-4 bg-primary-yellow h-24">Memperluas penyebaran dampak dengan pemanfaatan media informasi yang kredibel dan informatif</li>
+                </ol>
+            </div>
+            </div>
+
+
+            
+            </div>
         </div>
     </div>
     </section>
---
+++ src/sections/my-learning/Profile.jsx
@@ -0,0 +1,85 @@
+"use client"
+// import { getCurrentUser } from "@/commons/services/user/current";
+// import { cookies } from "next/headers";
+// import { redirect } from "next/navigation";
+// import { getUser } from "@/commons/services/user";
+// import { useState } from "react";
+const Profile = async(user) => {
+  if (user && user.user)  {
+    var profile = user.user
+    if (!profile.age) // tes
+      console.log("tes")
+  }
+  
+  return (
+    <main className="min-h-screen">
+      <div className="p-16">
+        <div className="p-8 bg-white shadow mt-24">
+          <div className="text-center">
+            {/* <div className="grid grid-cols-1 text-center order-last md:order-first mt-20 md:mt-0">
+              <div>
+                <p className="font-bold text-gray-700 text-xl">22</p>
+                <p className="text-gray-400">Friends</p>
+              </div>
+            </div> */}
+            <div className="relative">
+              <div className="w-24 h-24 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full p-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                </svg>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="mt-5 text-center border-b pb-12">
+            <h1 className="text-4xl font-medium text-gray-700">{profile.name}</h1>
+            <p className="font-light text-gray-600 mt-3">{profile.email}</p>
+
+          </div>
+          <div className="justify-center px-20 py-5 grid gap-10 grid-cols-1 sm:grid-cols-2">
+            <div className="h-max">
+              <p className="pb-2 font-light">Umur</p>
+              <div className="w-full h-full p-2 px-5 rounded-sm bg-primary-yellow bg-opacity-20  font-light">
+                {profile.age? 
+                <p className="text-sm font-semibold">{profile.age}</p>:
+                <p className="text-sm font-semibold">Belum ada data umur.</p>
+                }
+              </div> 
+            </div>
+            <div className="h-max">
+              <p className="pb-2 font-light">Jenis Kelamin</p>
+              <div className="w-full h-full py-2 px-5 rounded-sm bg-primary-yellow bg-opacity-20  font-light">
+                {profile.sex? 
+                <p className="text-sm font-semibold">{profile.sex}</p>:
+                <p className="text-sm font-semibold">Belum ada data jenis kelamin.</p>
+                }
+              </div> 
+            </div>
+            <div className="h-max">
+              <p className="pb-2 font-light">Domisili</p>
+              <div className="w-full h-full py-2 px-5 rounded-sm bg-primary-yellow bg-opacity-20  font-light">
+                {profile.domicile? 
+                <p className="text-sm font-semibold">{profile.domicile}</p>:
+                <p className="text-sm font-semibold">Belum ada data dommisili.</p>
+                }
+              </div> 
+            </div>
+            <div className="h-max">
+              <p className="pb-2 font-light">Pekerjaan</p>
+              <div className="w-full h-full py-2 px-5 rounded-sm bg-primary-yellow bg-opacity-20  font-light">
+                {profile.job? 
+                <p className="text-sm font-semibold">{profile.job}</p>:
+                <p className="text-sm font-semibold">Belum ada data pekerjaan.</p>
+                }
+              </div> 
+            </div>
+          </div>
+
+        </div>
+        </div>
+    </main>
+  );
+};
+
+export default Profile;
